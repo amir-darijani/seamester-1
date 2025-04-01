617e7bfe76c92549a4ced1573f1ddc2194b7d105
amir-darijani <87608463+amir-darijani@users.noreply.github.com>
2025-04-02 00:02:09 +0330

    Add files via upload

---
+++ jurny.cpp
@@ -0,0 +1,24 @@
+well 
+Hello
+this is about my jurni
+aculy about my plans on this travel
+firs of all i have one if or maby 2 ifs
+the first one is i dont now where am i going cuz Dr.nabi isnt home and mozhde &ebi are in there (khabgah)
+so can't go to Dr. house.
+a nother option is ali moshkis home hope i can get his adres ASAP and i can get snapp whene i get arive to the airplane;
+as we speek im on loot lake on air plane ,soooo butiful hope you guys see thise mirecal soon
+it seams i was wrong beacus the hosters sed after 10 min wwe shuld arive to tehran.
+at this moment i talk with you in in alis house at 10:20 O clock  8om
+1:go get some programming booke
+2:be more profetional at programing
+3:improve your english
+                                          
+
+
+
+
+
+
+                                                   this are things you shuld get at thi aresoad cant wate too see you,...................
+                                      
+ 
